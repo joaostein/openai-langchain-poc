0c62b00c8111b35b38833d13b034880cc7033847
Joao Stein <joaosteing@gmail.com>
2023-06-06 23:32:35 -0300

    feat: add langchain implementation

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-import { Configuration, OpenAIApi } from "openai";
+import { OpenAI } from "langchain/llms/openai";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { loadQAStuffChain } from "langchain/chains";
 import { config } from "dotenv";
 import fs from "fs";
 config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const model = new OpenAI();
+const sample = fs.readFileSync('./sample.txt', 'utf8');
+const query = 'what is the color of the sky in autorama?';
 
-const openai = new OpenAIApi(configuration);
-const context = fs.readFileSync('./sample.txt', 'utf8');
-const question = `What year can the roots of the Internet be traced back to?`;
-const prompt = `${context}\nQ: ${question}`;
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 500,
+  chunkOverlap: 100,
+});
 
 const main = async () => {
   try {
-    const response = await openai.createCompletion({
-      prompt: prompt,
-      max_tokens: 32,
-      temperature: 0,
-      model: 'text-davinci-003',
+    const docs = await splitter.createDocuments([sample]);
+    const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+    const result = await vectorStore.similaritySearch(query, 5);
+    const chain = new loadQAStuffChain(model);
+    const res = await chain.call({
+      question: query,
+      input_documents: result,
     });
-    console.log(response.data.choices[0].text);
+    console.log(res);
   } catch (error) {
     console.error(error);
   }
